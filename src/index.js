4ec1dad8ca0a758e3bb1a6f7443402ab6c697a08
Jay Fallon <jayfallon@gmail.com>
2019-09-02 21:49:05 -0400

    added grid component

--- src/index.js
+++ src/index.js
@@ -9,23 +9,58 @@ class Grid extends Component {
     this.state = {};
   }
   render() {
-    return <div>Grid</div>;
+    const width = this.props.cols * 14
+    let rowsArr = []
+    let boxClass = ""
+    for (let i = 0; i < this.props.rows; i++) {
+      for (let j = 0; j < this.props.rows; j++) {
+        let boxId = i + " " + j;
+
+        boxClass = this.props.gridFull[i][j] ? "box on" : "box off"
+        rowsArr.push(
+          <Box
+            boxClass={boxClass}
+            key={boxId}
+            row={i}
+            col={j}
+            selectBox={this.props.selectBox}
+          />
+        )
+      }
+    }
+
+
+    return <div className="grid" style={{width: width}}>
+      {{rowsArr}}
+    </div>;
   }
 }
 
 class Main extends Component {
   constructor(props) {
     super(props);
+    this.speed= 100;
+    this.rows= 30;
+    this.cols= 50;
+
     this.state = {
-      generation: null
+      generation: 0,
+      gridFull: Array(this.rows).fill().map(()=>Array(this.cols).fill(false))
     };
   }
   render() {
+    const {rows,cols} = this.props;
+    const {gridFull, generation} = this.state;
     return (
       <div>
         <h1>The Game of Life</h1>
-        <Grid />
-        <h2>generations: {this.state.generation}</h2>
+        <Grid 
+          gridFull={gridFull}
+          cols={cols}
+          rows={rows}
+          selectBox={this.selectBox}
+         />
+        <h2>generations: {generation}</h2>
       </div>
     );
   }
